3f8cdbca68e7c8c0fb148f5f13acb4b0b1259502
Dannirux <danniruxorellana@hotmail.com>
2022-07-28 10:19:45 -0500

    cambio de target

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  target: 'static',
   server: {
     port: process.env.PORT || 3000,
     host: '0.0.0.0',
@@ -64,5 +65,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+
   }
 }
